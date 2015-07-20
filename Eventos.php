c4878c79955ce5a4f3fd70966b1e2c8c4ecf838a
Fagundes <Fagundes@Fagundes-PC>
2015-07-20 14:54:11 -0300

    evts

--- Eventos.php
+++ Eventos.php
@@ -3,6 +3,15 @@ class Eventos {
 	public $id;
 	public $nome;
 	
+
+	public function setId($id) {
+		$this->id = $id;
+	}
+	
+	public function getId($id) {
+		return $this->id;
+	}
+	
 	
 	public function setNome($nome) {
 		$this->nome = $nome;
